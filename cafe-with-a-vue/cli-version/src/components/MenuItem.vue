5e2313477127b12cb00b341addcc0ffa443e086d
msenard <msenard@btslivh.eu>
2023-02-01 14:16:06 +0100

    Ajout de l'exercice sur les directives

---
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -0,0 +1,35 @@
+<template>
+    <h2>Menu</h2>
+    <div v-for="item in simpleMenu" :key="item.name" class="menu-item">
+        <img
+            class="menu-item__image"
+            v-bind:src="item.image.source"
+            v-bind:alt="item.image.alt"
+        />
+        <div>
+            <h3>{{ item.name }}</h3>
+            <p v-if="item.inStock">En stock</p>
+            <p v-else>En rupture de stock</p>
+            <div>
+                <label for="add-item-quantity"
+                    >Quantité : {{ item.quantity }}</label
+                >
+                <input
+                    v-model.number="item.quantity"
+                    id="add-item-quantity"
+                    type="number"
+                />
+                <button @click="addToShoppingCart(item.quantity)">
+                    Ajouter au panier d'achat
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MenuItem"
+        props:
+    }
+</script>
